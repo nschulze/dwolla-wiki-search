067ce81992dc9e08ecbd19dda2cb6f3df0b8723d
Nick Schulze <nicks@dwolla.com>
2018-05-17 14:54:27 -0500

    add dwolla wiki search

--- searches.js
+++ searches.js
@@ -1,6 +1,7 @@
 const searches = {
   amazon: { name: 'Amazon', url: 'https://www.amazon.com/s?url=search-alias=aps&field-keywords='},
   duck: { name: 'DuckDuckGo', url: 'https://duckduckgo.com/?q='},
+  dwolla: { name: 'Dwolla', url: 'https://sites.google.com/a/dwolla.com/wiki/system/app/pages/search?scope=search-site&q='},
   giphy: { name: 'Giphy', url: 'https://giphy.com/search/'},
   gh: { name: 'GitHub', url: 'https://github.com/search?utf8=%E2%9C%93&q='},
   google: { name: 'Google', url: 'https://www.google.com/search?q='},
