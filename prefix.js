74bdc15f83e34fc2676ec45b8d585ba6943335f3
Nick Schulze <nicks@dwolla.com>
2018-05-24 17:36:24 -0500

    stash search

--- prefix.js
+++ prefix.js
@@ -27,10 +27,10 @@ module.exports = (pluginContext) => {
             }).then(res => {
                const jsonBody = JSON.parse(res.body)
                return jsonBody.values.map(x => ({
-                   icon: x.project.avatarUrl,
-                   title: `${x.name} + ${term}`,
+                   icon: x.project.avatarUrl || path.join('assets', prefix + '.png'),
+                   title: `${x.name} + ${term} + `,
                    subtitle: x.links.self[0].href,
-                   value: x.links.self[0].href
+                   value: url
                }))
            }))
         }
