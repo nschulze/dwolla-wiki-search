1d1be0e0eba9fa060965c8473589f69b5d99337b
Nick Schulze <nicks@dwolla.com>
2018-11-09 14:32:57 -0600

    add bitbucket support

--- prefix.js
+++ prefix.js
@@ -41,17 +41,27 @@ module.exports = (pluginContext) => {
             const terms = term.split(' ')
             var modifier = terms[1] ? terms[1] : ''
             modifier = autofill(modifier);
-            const url = `https://bitbucket.dwolla.net/rest/api/latest/repos/?avatarSize=32&start=0&limit=20&name=${terms[0]}&projectname=`
-           return Promise.resolve(got(url, {
-            }).then(res => {
-               const jsonBody = JSON.parse(res.body)
-               return jsonBody.values.map(x => ({
-                   icon: path.join('assets', prefix + '.png'),
-                   title: getTitle(x.name, modifier),
-                   subtitle: getUrl(x.links, modifier),
-                   value: getUrl(x.links, modifier)
-               }))
-           }))
+            if (terms[0] === 'dashboard') {
+                 return jsonBody.values.map(x => ({
+                     icon: path.join('assets', prefix + '.png'),
+                     title: 'Bitbucket',
+                     subtitle: 'Dashboard',
+                     value: 'https://bitbucket.dwolla.net/dashboard'
+                 }))
+            }
+            else {
+              const url = `https://bitbucket.dwolla.net/rest/api/latest/repos/?avatarSize=32&start=0&limit=20&name=${terms[0]}&projectname=`
+              return Promise.resolve(got(url, {
+              }).then(res => {
+                 const jsonBody = JSON.parse(res.body)
+                 return jsonBody.values.map(x => ({
+                     icon: path.join('assets', prefix + '.png'),
+                     title: getTitle(x.name, modifier),
+                     subtitle: getUrl(x.links, modifier),
+                     value: getUrl(x.links, modifier)
+                 }))
+             }))
+           }
         }
       return Promise.resolve(
         [
